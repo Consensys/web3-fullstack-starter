facb3cec6eb3475674cbc12e629b87cdb09044b2
Alejandro <smarketing.cuz@gmail.com>
2024-10-04 09:59:51 -0500

    Moved web to packages/site directory

--- web/src/types.ts
+++
@@ -1,6 +0,0 @@
-import { AbiParametersToPrimitiveTypes, ExtractAbiFunction } from "abitype";
-import { ballotsAbi } from "@/lib/abis";
-
-export type Ballots = AbiParametersToPrimitiveTypes<
-  ExtractAbiFunction<typeof ballotsAbi, "getBallots">["outputs"]
->["0"];
